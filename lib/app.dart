fd42988cbae3d49217addd278a1f8c2526494d09
Kalandin Gleb <gkalandin@croc.ru>
2022-01-31 22:18:32 +0300

    Firebase initialization

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:pimp_my_qr/auth_screen/auth_screen.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
